be107f641e3ba159ffa0e958903259d438665b8f
nasonmamu <nasonmamu94@gmail.com>
2023-09-07 05:44:07 -0700

    Hello, puts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistdio.h>
+/**
+* main - A c program that prints a line to the standard error
+* return 1 (success)
+*/
+int main(void)
+{
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
+return (1);
+}
+
