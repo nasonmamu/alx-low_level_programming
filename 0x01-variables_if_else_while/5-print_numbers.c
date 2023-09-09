a0d66fcd2620b859221c975359af739585f863ea
nasonmamu <nasonmamu94@gmail.com>
2023-09-09 02:14:35 -0700

    a program that prints all single digit numbers of base 10 starting from 0, followed by a new line.
    All your code should be in the main function

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*
+ *main - to print the specified string
+
+ *Return : 0 to exit
+
+*/
+int main(void)
+
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
