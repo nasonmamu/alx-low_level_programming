15d2d819cb0c6c810d7149328ecf7fd2ebba41f0
nasonmamu <nasonmamu94@gmail.com>
2023-09-13 11:13:32 -0700

    Removed the invalid arguments

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,28 +2,16 @@
 #include <unistd.h>
 
 /*
- *main-Entry point
  *_putchar - writes the character c to stdout
- *@c: The character to print
+ * @c: The character to print
  *
  * Description:program that prints _putchar
  *
- *Return : Always 0 (Success)
- *Return: On success 1.
- *On error, -1 is returned, and errno is set appropriately.
+ * Return : Always 0 (Success)
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
 */
-
-		int main(void)
-		int _putchar(char c)
+int _putchar(char c)
 {
-		char str[] = "_putchar";
-		int ch;
-
-		for (ch = 0; ch < 8; ++ch)
-		_putchar(str[ch]);
-		_putchar('\n');
-
-		return (0);
-		return (write(1, &c, 1));
+	return (write(1, &c, 1));
 }
-
