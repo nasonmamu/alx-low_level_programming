1826bc91cb417846c81a81fb4ad8424ece724984
nasonmamu <nasonmamu94@gmail.com>
2023-09-13 11:01:16 -0700

    Created the masin file with the function prototypes

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,14 +1,25 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File : main.h
+ * Auth : Nason Mamu
+ * Desc : Header file containing function declarations and prototypes
+ * used in 0x02-functions_nested_loops.
+ */
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
-int _isalpha(int c); 
+int _isalpha(int c);
 int print_sign(int n)
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
-void tames_table(void);
+void times_table(void);
 int add(int, int);
-void print_to_90(int n);
+void print_to_98(int n);
 void print_times_table(int n);
 
+#endif
