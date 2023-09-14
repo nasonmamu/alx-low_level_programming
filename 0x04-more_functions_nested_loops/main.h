efe42576790cdfa72ae3094f88781b63ab0dcabc
nasonmamu <nasonmamu94@gmail.com>
2023-09-14 01:24:53 -0700

    function that checks for uppercase character

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File : main.h
+ * Auth : Nason Mamu
+ * Desc : Header file containing function declarations and prototypes
+ * used in 0x04. C - More functions, more nested loops.
+ */
+
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+
+#endif    
