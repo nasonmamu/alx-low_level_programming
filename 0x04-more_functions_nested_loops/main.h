5fbda575148cc71b778130ec219a11d8a3af5827
nasonmamu <nasonmamu94@gmail.com>
2023-09-16 02:13:33 -0700

    added putchar prototype

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -8,11 +8,11 @@
  * used in 0x04. C - More functions, more nested loops.
  */
 
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
-int mul(int a, int b);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
