703604d409683a6fe7e6876f76acdb4a839acb03
nasonmamu <nasonmamu94@gmail.com>
2023-09-18 06:13:18 -0700

    removed semicolon at the end of the line

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,9 +1,16 @@
-#Ifdefine MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
+
+/*
+ * File : main.h
+ * Auth : Nason Mamu
+ * Desc : Header file containing function declarations and prototypes
+ * used in 0x05. C - Pointers, arrays and strings.
+ */
 
 #include <stdio.h>
 #include <unistd.h>
-int _putchar(char c);;
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
