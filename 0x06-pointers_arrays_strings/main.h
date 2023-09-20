49d897750d0a4ea9aab2fdd2034867284a479127
nasonmamu <nasonmamu94@gmail.com>
2023-09-20 02:30:39 -0700

    added putchar

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,6 +7,7 @@
  * Desc : Header file containing function declarations and prototypes
  * used in 0x06. C - More pointers, arrays and strings.
  */
+int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
