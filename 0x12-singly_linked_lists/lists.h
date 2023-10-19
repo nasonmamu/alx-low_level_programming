087255677c15a766e4a90d4865a261612ff9a7c0
nasonmamu <nasonmamu94@gmail.com>
2023-10-19 13:27:50 -0700

    removed spaces

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,4 +1,4 @@
-#ifndiief LISTS_H
+#ifndef LISTS_H
 #define LISTS_H
 
 /*
@@ -32,4 +32,4 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-#endif
+#endifi
